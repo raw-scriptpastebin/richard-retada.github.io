993e9f146bdbae98b9225522802e3dba0051b0a9
Richard Retada <114992634+RICHARD-RETADA@users.noreply.github.com>
2022-10-05 15:10:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
       document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
       
-      document.querySelector("date").textContent = "DAY "+Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 12);
+      document.querySelector("date").textContent = "DAY "+Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24);
 
       function olock() {
             var today = new Date(),
-            hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
+            hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 12,
             min = (Math.floor( Math.floor((today - yourDate) / 1000) / 60)) % 60,
             sec =  Math.floor((today - yourDate) / 1000) % 60;
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
